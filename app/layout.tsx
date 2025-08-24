7baf707e4b30d03d249e89225b481f6d8594dafc
Zou Minowa <zou.minowa@shoppermediagroup.com>
2025-08-24 15:30:48 +0000

    Add resend

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,28 @@
+import "@radix-ui/themes/styles.css";
+import { Flex, Text, Theme } from "@radix-ui/themes";
+
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      {/* Reset default margin to position copyright with flexbox. */}
+      <body style={{ margin: 0 }}>
+        <Theme accentColor="gray">
+          <Flex direction="column" style={{ minHeight: "100vh" }}>
+            <div style={{ flexGrow: 1 }}>{children}</div>
+            <footer>
+              <Flex justify="center">
+                <Text size="1">
+                  © {new Date().getFullYear()} Victor Five. All rights reserved.
+                </Text>
+              </Flex>
+            </footer>
+          </Flex>
+        </Theme>
+      </body>
     </html>
-  )
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,62 @@
+import { Resend } from "resend";
+import {
+  Box,
+  Button,
+  Container,
+  Heading,
+  Section,
+  Text,
+  VisuallyHidden,
+} from "@radix-ui/themes";
+
 export default function Page() {
-  return <h1>Hello, Next.js!</h1>
+  const submit = async () => {
+    "use server";
+
+    const resend = new Resend(process.env.RESEND_API_KEY);
+
+    try {
+      const { data } = await resend.emails.send({
+        from: "onboarding@resend.dev",
+        to: "zouminowa@gmail.com",
+        subject: "Hello",
+        html: "<h1>Hello</h1>",
+      });
+
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <Container m="5">
+      <header>
+        <Heading size="9" align="right">
+          VICTOR FIVE
+        </Heading>
+      </header>
+      <main>
+        <Section>
+          <VisuallyHidden>
+            <Heading as="h2">Bio</Heading>
+          </VisuallyHidden>
+          <Text>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti
+            iure quae impedit ratione libero. Quae fugiat labore quam quos sit
+            nihil, culpa tenetur molestias earum expedita iure rem? Esse, rerum.
+          </Text>
+        </Section>
+        <Section>
+          <Box>
+            <form action={submit}>
+              <Button type="submit" variant="outline">
+                Send email
+              </Button>
+            </form>
+          </Box>
+        </Section>
+      </main>
+    </Container>
+  );
 }
