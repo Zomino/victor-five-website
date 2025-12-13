450b5df7dac27201552eeda1009df23ea65d008e
Zou Minowa <zouminowa@gmail.com>
2025-12-13 17:15:52 +0000

    Update button text

--- app/booking/page.tsx
+++ app/booking/page.tsx
@@ -88,7 +88,7 @@ export default function Page() {
         </Box>
         <Box component="main" m="lg" mt="xl">
           <Title order={2} ta="center">
-            Request Booking
+            Book 1:1 Session – Zurich
           </Title>
           <Box mt="md">
             <BookSessionForm onSubmit={handleSubmit} />
