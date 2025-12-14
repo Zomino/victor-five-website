328b38322f635ff67261c5d2142cde325cecc79d
Zou Minowa <zouminowa@gmail.com>
2025-12-14 15:35:48 +0000

    Add German U

--- app/booking/page.tsx
+++ app/booking/page.tsx
@@ -76,7 +76,7 @@ export default function Page() {
         </Box>
         <Box component="main" m="lg">
           <Title order={2} ta="center">
-            Book 1:1 Session - Zurich
+            Book 1:1 Session - Zürich
           </Title>
           <Box mt="md">
             <BookSessionForm onSubmit={handleSubmit} />
